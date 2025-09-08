62dd4ee67a994633e9db1653e39e709accade52c
raitako-1 <raiji.kure551.naitoki@gmail.com>
2025-09-08 18:10:13 +0900

    Fix subscription bug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 console.log('Starting...')
 
+import { initIngester } from 'atingester'
 import { closeSignal } from './cmds/stop'
 import FeedGenerator from './server'
 import { setupCmd } from './util/cmd'
@@ -11,6 +12,8 @@ const run = async () => {
   logger.info(`Running ${process.env.npm_package_name} ${process.env.npm_package_version} (${env.NODE_ENV})`)
   logger.info(`System Info: Node.js ${process.version} / ${process.platform} ${process.arch}`)
   logger.debug('DebugMode is enabled.')
+
+  await initIngester()
 
   const server = await FeedGenerator.create()
 
